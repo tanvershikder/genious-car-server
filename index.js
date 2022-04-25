2e2f7406caf7a474a4c908166f6de030c0b4bb39
Your Name <you@example.com>
2022-04-26 00:32:45 +0600

    done withe module 68

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -11,26 +12,55 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-
+// verify token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unothorized user" })
+    }
+    const token = authHeader.split(" ")[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ message: "forbiden Access" })
+        }
+        console.log("decoded ", decoded);
+        req.decoded = decoded;
+        next()
+    })
+    
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('genious-car-service').collection('services');
 
-        app.get('/service',async (req,res)=>{
+        const orderCollection = client.db('genious-car-service').collection('order');
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send(accessToken)
+        })
+
+        //get service api
+
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
 
-        app.get('/service/:id' , async(req,res)=>{
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query);
             res.send(service)
             console.log("lodad");
@@ -38,7 +68,7 @@ async function run(){
 
 
         // post newService
-        app.post('/service',async (req,res)=>{
+        app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result)
@@ -47,25 +77,48 @@ async function run(){
 
         //delete service
 
-        app.delete('/service/:id', async(req,res)=>{
+        app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query);
             res.send(result)
         })
 
+
+        // api for order collection 
+
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = orderCollection.find(query);
+                const order = await cursor.toArray();
+                res.send(order)
+            }
+            else{
+                res.status(403).send({message: "forbiden access"})
+            }
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+
     }
-    finally{
+    finally {
         // await client.close(); if i want to stop connection after need of database
     }
 }
 run().catch(console.dir)
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('geinious car service server is running')
 })
 
-app.listen(port,()=>{
-    console.log("lisening port :",port);
+app.listen(port, () => {
+    console.log("lisening port :", port);
 })
